b1d0b1d2bfbeda17d10fbd2940c9036c780254ab
Engin Paksoy <96542214+enginpaksoy@users.noreply.github.com>
2023-08-18 23:11:33 +0300

    Create 469A - I Wanna Be the Guy.c

---
+++ 469A - I Wanna Be the Guy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int array[100] = {0};
+    int p,q;
+    int level;
+    scanf("%d", &p);
+    for (int i = 0; i < p; ++i) {
+        scanf("%d", &level);
+        array[level] = 1;
+    }
+    scanf("%d", &q);
+    for (int i = 0; i < q; ++i) {
+        scanf("%d", &level);
+        array[level] = 1;
+    }
+    for (int i = 1; i <= n; ++i) {
+        if(array[i] == 0){
+            printf("Oh, my keyboard!");
+            return 0;
+        }
+    }
+    printf("I become the guy.");
+}
