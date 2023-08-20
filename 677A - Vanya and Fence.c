5dc492e4e6ba96ce15237c18f218e4e3a795f16b
Engin Paksoy <96542214+enginpaksoy@users.noreply.github.com>
2023-08-20 14:19:28 +0300

    Update 677A - Vanya and Fence.c

--- 677A - Vanya and Fence.c
+++ 677A - Vanya and Fence.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-#define SIZE 1000
 
-int main(){
-    int num, fence;
-    scanf("%d %d", &num, &fence);
-    int friends[SIZE] = {};
-    int counter = 0;
-    for (int i = 0; i < num; ++i) {
+int main() {
+    int n, h;
+    scanf("%d%d", &n, &h);
+    int friends[n];
+    int width_road = 0;
+    for (int i = 0; i < n; ++i) {
         scanf("%d", &friends[i]);
-    }
-    for (int j = 0; j < num; ++j) {
-        if(friends[j] > fence){
-            counter += 2;
+        if(friends[i] > h){
+            width_road += 2;
         }
-        else if(friends[j] <= fence){
-            counter++;
+        else{
+            width_road++;
         }
     }
-    printf("%d", counter);
+    printf("%d", width_road);
 }
